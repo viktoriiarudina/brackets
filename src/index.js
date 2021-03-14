82f1af053a73a385b303eb215add103a890d3f53
Viktoriia <viktoria.nosik@gmail.com>
2021-03-14 22:30:15 +0100

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(typeof str !== 'string'){
+        return false;
+    }
+
+    var open = [];
+    var close = [];
+    var bufer = [];
+    var check = true;
+    bracketsConfig.map(el => open.push(el[0]));
+    bracketsConfig.map(el => close.push(el[1]));
+
+    str.split('').forEach(el => {
+        if (open.includes(el) && close.includes(el)) {
+            if(!bufer.includes(el)){
+                bufer.push(el);
+                return
+            } else if(bufer.includes(el) && bufer[bufer.length-1] === el){
+                bufer.pop()
+                return;
+            } else if(bufer.includes(el) && bufer[bufer.length-1] === el){
+                check = false;
+            }
+        }
+        if(close.includes(el)){
+          close.indexOf(el) === open.indexOf(bufer[bufer.length-1]) ? bufer.pop() : check = false;
+          return;
+        }
+        bufer.push(el);
+    })
+
+    return bufer.length === 0 && check ? true : false;
 }
